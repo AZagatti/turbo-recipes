243971c620b8a5129bc233de32d37f0471e023b3
azagatti <andre.zagatti1@gmail.com>
2025-07-24 21:35:17 -0300

    feat: initial project setup

---
+++ src/main/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => {
+  return { message: 'Hello, World!' }
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 Server listening on port 3333')
+})
